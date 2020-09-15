bbadd2d87db83408fac6c997571c9616d23978c4
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2020-09-15 20:02:32 +0530

    Update services.dart

--- lib/services.dart
+++ lib/services.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const apiKey = "105BB565-35BD-460F-A2C8-2FCEC253E3F6";
+const apiKey = "Your API goes here";
 const apiUrl = "http://rest.coinapi.io/v1/exchangerate";
 // this function accept currency and cryptocurrency and returns the current rate accordingly
 Future<String> getRate(String cur, String crypto) async{
